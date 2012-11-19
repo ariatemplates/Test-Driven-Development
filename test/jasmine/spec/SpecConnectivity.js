877605e44215c240a13f42f484acc422413b0715
Fabio Crisci <fabio.crisci@amadeus.com>
2012-11-19 13:28:34 +0100

    Test event emitter
    First test on event emitter written in Jasmine

---
+++ test/jasmine/spec/SpecConnectivity.js
@@ -0,0 +1,64 @@
+beforeEach(function () {
+    this.addMatchers({
+        toBeConnected : function () {
+            this.message = function () {
+                return "Expected connectivity state to be " + this.isNot ? "disconnected" : "connected";
+            };
+
+            return this.actual.getState();
+        }
+    });
+
+    // Always start with a connected network
+    Connectivity.setState(true);
+    // And remove any callbacks
+    Connectivity.off();
+});
+
+describe("connectivity", function () {
+    it("should raise an event when connectivity changes", function () {
+        var eventCallback = jasmine.createSpy("eventCallback");
+
+        Connectivity.on("connectivityChange", eventCallback);
+
+        Connectivity.setState(false);
+
+        expect(Connectivity).not.toBeConnected();
+
+        expect(eventCallback).toHaveBeenCalled();
+
+        // Set again to false -> souldn't call the callback again, because the state is not changing
+        Connectivity.setState(false);
+
+        expect(eventCallback.calls.length).toEqual(1);
+
+        // And now go to true, should call the callback
+        Connectivity.setState(true);
+
+        expect(eventCallback.calls.length).toEqual(2);
+    });
+
+    it("shouldn't raise an event after calling off", function () {
+        var callbacks = {
+            cb1 : function () {},
+            cb2 : function () {},
+            cb3 : function () {}
+        };
+
+        spyOn(callbacks, "cb1");
+        spyOn(callbacks, "cb2");
+        spyOn(callbacks, "cb3");
+
+        Connectivity.on("connectivityChange", callbacks.cb1);
+        Connectivity.on("connectivityChange", callbacks.cb2);
+        Connectivity.on("connectivityChange", callbacks.cb3);
+
+        Connectivity.off("connectivityChange", callbacks.cb2);
+
+        Connectivity.setState(false);
+
+        expect(callbacks.cb1).toHaveBeenCalled();
+        expect(callbacks.cb2).not.toHaveBeenCalled();
+        expect(callbacks.cb3).toHaveBeenCalled();
+    });
+});
