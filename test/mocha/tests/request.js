91c584e800d439b561ac996102fb7fe1901453b9
Fabio Crisci <fabio.crisci@amadeus.com>
2012-11-27 14:14:42 +0100

    Refactor test to include fail callback

--- test/mocha/tests/request.js
+++ test/mocha/tests/request.js
@@ -1,26 +1,36 @@
 describe("Working connection", function () {
     var clock;
+    var callbacks;
     before(function () {
         clock = sinon.useFakeTimers();
 
         // This adapter always makes the connection pass
         Connectivity.Adapter = {
             send : function (request) {
-                var later = null;
+                var later = {
+                    success : null,
+                    failure : null
+                };
 
                 setTimeout(function () {
-                    if (later) {
-                        later.call(null, {
+                    if (request.url === "/success") {
+                        later.success.call(null, {
                             status : 200,
                             responseText : "OK"
+                        });
+                    } else {
+                        later.failure.call(null, {
+                            status : 404,
+                            responseText : ""
                         });
                     }
                 }, 50);
 
                 // Basic implementation of promises!
                 return {
-                    then : function (success) {
-                        later = success;
+                    then : function (success, fail) {
+                        later.success = success;
+                        later.failure = fail;
                     }
                 };
             }
@@ -29,16 +39,14 @@ describe("Working connection", function () {
     after(function () {
         clock.restore();
     });
+    beforeEach(function () {
+        callbacks = {
+            success : sinon.spy(),
+            failure : sinon.spy()
+        };
+    });
 
     it("should call the success callback", function () {
-        var callbacks = {
-            success : function () {},
-            failure : function () {}
-        };
-
-        sinon.spy(callbacks, "success");
-        sinon.spy(callbacks, "failure");
-
         var request = {
             method : "GET",
             url : "/success"
@@ -58,6 +66,29 @@ describe("Working connection", function () {
         expect(callbacks.success.firstCall.args[1]).to.eql({
             status : 200,
             responseText : "OK"
+        });
+    });
+
+    it("should call the fail callback", function () {
+        var request = {
+            method : "GET",
+            url : "/fail"
+        };
+
+        Connectivity.send(request).then(callbacks.success, callbacks.failure);
+
+        expect(callbacks.failure.called).not.to.be.ok();
+
+        // Let the time flow
+        clock.tick(100);
+
+        expect(callbacks.success.called).not.to.be.ok();
+        expect(callbacks.failure.calledOnce).to.be.ok();
+
+        expect(callbacks.failure.firstCall.args[0]).to.eql(request);
+        expect(callbacks.failure.firstCall.args[1]).to.eql({
+            status : 404,
+            responseText : ""
         });
     });
 });
