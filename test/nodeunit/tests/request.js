fe2c36f89aadef38175a85947d4f33a067ae6267
Fabio Crisci <fabio.crisci@amadeus.com>
2012-11-28 11:51:53 +0100

    Test fail callback

--- test/nodeunit/tests/request.js
+++ test/nodeunit/tests/request.js
@@ -13,21 +13,30 @@
             // This adapter always makes the connection pass
             Connectivity.Adapter = {
                 send : function (request) {
-                    var later = null;
+                    var later = {
+                        success : null,
+                        failure : null
+                    };
 
                     setTimeout(function () {
-                        if (later) {
-                            later.call(null, {
+                        if (request.url === "/success") {
+                            later.success.call(null, {
                                 status : 200,
                                 responseText : "OK"
+                            });
+                        } else {
+                            later.failure.call(null, {
+                                status : 404,
+                                responseText : ""
                             });
                         }
                     }, 50);
 
                     // Basic implementation of promises!
                     return {
-                        then : function (success) {
-                            later = success;
+                        then : function (success, fail) {
+                            later.success = success;
+                            later.failure = fail;
                         }
                     };
                 }
@@ -64,6 +73,33 @@
             test.deepEqual(callbacks.success.firstCall.args[1], {
                 status : 200,
                 responseText : "OK"
+            });
+
+            test.done();
+        },
+
+        fail : function (test) {
+            test.expect(5);
+
+            var request = {
+                method : "GET",
+                url : "/fail"
+            };
+
+            Connectivity.send(request).then(callbacks.success, callbacks.failure);
+
+            test.ok(!callbacks.failure.called);
+
+            // Let the time flow
+            clock.tick(100);
+
+            test.ok(!callbacks.success.called);
+            test.ok(callbacks.failure.calledOnce);
+
+            test.deepEqual(callbacks.failure.firstCall.args[0], request);
+            test.deepEqual(callbacks.failure.firstCall.args[1], {
+                status : 404,
+                responseText : ""
             });
 
             test.done();
