740e39d035dcbd41430796ae86c7ec5fdcb9b1cb
Fabio Crisci <fabio.crisci@amadeus.com>
2012-12-11 14:06:41 +0100

    Test request fail

--- test/aria-templates/tests/Adapter.js
+++ test/aria-templates/tests/Adapter.js
@@ -21,6 +21,9 @@
             server.respondWith("/success", [200, {
                         "Content-Type" : "application/json"
                     }, '[{ "ok": true }]']);
+            server.respondWith("/fail", [404, {
+                        "Content-Type" : "application/json"
+                    }, '[{ "ok": false }]']);
             server.autoRespondAfter = 100;
         },
         $destructor : function () {
@@ -65,6 +68,30 @@
 
                 this.assertTrue(callbacks.success.calledOnce);
                 this.assertFalse(callbacks.failure.called);
+                this.assertFalse(callbacks.timeout.called);
+            },
+
+            testFailCallback : function () {
+                var request = {
+                    url : "/fail",
+                    timeout : 1000
+                };
+
+                Connectivity.Adapter.send(request).then(callbacks.success, callbacks.failure, callbacks.timeout);
+
+                // Time for a request
+                clock.tick(100);
+                server.respond();
+
+                this.assertFalse(callbacks.success.called);
+                this.assertTrue(callbacks.failure.calledOnce);
+                this.assertFalse(callbacks.timeout.called);
+
+                // Let the timeout run
+                clock.tick(1000);
+
+                this.assertFalse(callbacks.success.called);
+                this.assertTrue(callbacks.failure.calledOnce);
                 this.assertFalse(callbacks.timeout.called);
             }
         }
